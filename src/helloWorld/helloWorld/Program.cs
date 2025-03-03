c1c537b3e13e34362f8679308cea49044d659370
Zaki Alfikri <zakialfikri313@gmail.com>
2025-03-03 11:48:10 +0700

    new file

---
+++ src/helloWorld/helloWorld/Program.cs
@@ -0,0 +1,10 @@
+﻿cnamespace helloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
